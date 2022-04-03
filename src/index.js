3586ad98b763ab13c800b8ee85a647d9ccde3b18
Tej-Pawar <tejaspawr073@gmail.com>
2022-04-03 15:13:34 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require("express");
+const hbs = require("hbs")
+const app = express();
+app.set("view engine", "hbs");
+const templetepath = path.join(__dirname, "../templete/views");
+app.set("views", templetepath);
+const partialpath = path.join(__dirname, "../templete/partial");
+hbs.registerPartials(partialpath);
+const port = 8000;
+const staticpath = path.join(__dirname, "../public")
+app.use(express.static(staticpath))
+app.get('/', (req, resp) => {
+    resp.render("index");
+})
+app.get('/about', (req, resp) => {
+    resp.render("about");
+})
+app.get('/whether', (req, resp) => {
+    resp.render("whether");
+})
+app.get('*', (req, resp) => {
+    resp.render("404");
+})
+app.listen(port, () => {
+    console.log(`port is listen on ${port}`);
+})
