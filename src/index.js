3c8947eb077965344cefdcec267dc4d9d10a0218
Tej-Pawar <tejaspawr073@gmail.com>
2022-04-03 22:16:56 +0530

    port num change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ const path = require('path');
 const express = require("express");
 const hbs = require("hbs")
 const app = express();
+const port = process.env.PORT || 8000;
 app.set("view engine", "hbs");
 const templetepath = path.join(__dirname, "../templete/views");
 app.set("views", templetepath);
 const partialpath = path.join(__dirname, "../templete/partial");
 hbs.registerPartials(partialpath);
-const port = 8000;
+
 const staticpath = path.join(__dirname, "../public")
 app.use(express.static(staticpath))
 app.get('/', (req, resp) => {
